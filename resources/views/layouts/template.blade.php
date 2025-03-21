cc0d70063766227d77917d1abc5b6defec830982
Delinda Candrawati Eka Putri <delindap26@gmail.com>
2025-03-22 01:24:47 +0700

    layouts awal

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,64 @@
+<!--
+=========================================================
+* Material Dashboard 3 - v3.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('layouts.header')
+</head>
+
+<body class="g-sidenav-show  bg-gray-100">
+    <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-radius-lg fixed-start ms-2  bg-white my-2"
+        id="sidenav-main">
+        @include('layouts.sidebar')
+    </aside>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        <!-- Navbar -->
+        <nav class="navbar navbar-main navbar-expand-lg px-0 mx-3 shadow-none border-radius-xl" id="navbarBlur"
+            data-scroll="true">
+            @include('layouts.navbar')
+        </nav>
+        <!-- End Navbar -->
+        <div class="content">
+            @yield('content')
+        </div>
+        <div class="container-fluid py-2">
+            <footer class="footer py-4">
+                @include('layouts.footer')
+            </footer>
+        </div>
+    </main>
+
+    <!--   Core JS Files   -->
+    <script src="{{ asset('template/assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('template/assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('template/assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('template/assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
+    <script>
+        var win = navigator.platform.indexOf('Win') > -1;
+        if (win && document.querySelector('#sidenav-scrollbar')) {
+            var options = {
+                damping: '0.5'
+            }
+            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+        }
+    </script>
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="{{ asset('template/assets/js/material-dashboard.min.js?v=3.2.0') }}"></script>
+</body>
+
+</html>
